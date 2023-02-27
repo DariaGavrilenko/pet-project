d736ffea71ec2a4a8bdc4097461e2c53fc29abab
DariaGavrilenko <gavrilenko10919@gmail.com>
2023-02-27 10:38:19 +0300

    test

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,16 +3,37 @@ import Vector from '../../assets/img/Vector.png';
 import logo from '../../assets/img/logo.png';
 import search from '../../assets/img/Shape.png';
 import { Button } from 'components/shared/Button';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const allCategories = ['electronics', 'jewelery', "men's clothing", "women's clothing"];
   return (
     <div className={s.headerContainer}>
       <div className={s.headerContentContainer}>
         <div className={s.logoContainer}>
           <img src={logo} alt="logo" />
           <p className={s.logoName}>crypter</p>
-          <p className={s.headerLink}>Discover</p>
-          <p className={s.headerLink}>How it work</p>
+          <p
+            className={s.headerLink}
+            onClick={() => {
+              setIsOpen(true);
+            }}>
+            All
+          </p>
+          {isOpen && (
+            <div
+              onBlur={() => {
+                alert('m');
+              }}
+              className={s.allCategoriesContainer}>
+              {allCategories.map((category, index) => (
+                <a href="" key={index}>
+                  {category}
+                </a>
+              ))}
+            </div>
+          )}
         </div>
         <div className={s.searchContainer}>
           <div className={s.inputContainer}>
